cae90728f18764395e17618b0f48f894014d03c2
Luis Rojas <luis.rojas@segic.usach.cl>
2021-04-13 15:24:38 -0400

    Prueba con insersion

--- code/db-basico.php
+++ code/db-basico.php
@@ -11,5 +11,21 @@ try{
        echo $e->getMessage();
 }
 
+$sql="INSERT INTO Users (full_name, email, user_name, password)
+      VALUES(:full_name, :email, :user_name, :password)";
 
+//statemest
 
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan Perez';
+$email = 'juan.perez@segic.cl';
+$user_name = 'juan.perez';
+$password = 'juan123'
+
+$stmt ->bindParam(':full_name',$full_name);
+$stmt ->bindParam(':email',$email);
+$stmt ->bindParam(':user_name',$user_name);
+$stmt ->bindParam(':password',$password);
+
+$stmt -> execute();
