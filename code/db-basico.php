90587b53b17f83496bec29c46a01e4144ccd0536
Luis Rojas <luis.rojas@segic.usach.cl>
2021-04-14 17:59:20 -0400

    Se mejora legibilidad de los indices

--- code/db-basico.php
+++ code/db-basico.php
@@ -9,7 +9,53 @@ try{
    $db = new PDO($dsn, $dbuser, $dbpassword);
 } catch(PDOException $e) {
        echo $e->getMessage();
-}
+
+       
+//Preparar consulta
+
+/*$sql="INSERT INTO users 
+               (full_name, email, user_name, password)
+      VALUES(:full_name, :email, :user_name, :password)";
+
+      //statemest
+
+$stmt = $db->prepare($sql);
+
+$full_name = 'Juan Perez';
+$email = 'juan.perez@segic.cl';
+$user_name = 'juan.perez';
+$password= password_hash('juan123', PASSWORD_DEFAULT);
+
+$stmt->bindParam(':full_name',$full_name);
+$stmt->bindParam(':email',$email);
+$stmt->bindParam(':user_name',$user_name);
+$stmt->bindParam(':password',$password);
+
+$stmt -> execute();*/
+
+//delete
+
+/*$id = 3;
+
+$stmt = $db->prepare("DELETE FROM users WHERE id=:id");
+$stmt->bindparam(':id',$id);
+$stmt->execute();*/
+
+
+//Insert masivo
+
+$users = [
+   [_'name'=>'Miguel Perez', 
+     'email'=>'miguel.perez@segic.cl', 
+     'user_name'=>'miguel.perez',
+     'password'=>'miguel123'
+   ],      
+   [ 'name'=>'Andrea Perez', 
+     'email'=>'andrea.perez@segic.cl', 
+     'user_name'=>'andrea.perez',
+     'password'=>'andrea123'
+   ],
+];
 
 $sql="INSERT INTO users (full_name, email, user_name, password)
       VALUES(:full_name, :email, :user_name, :password)";
@@ -18,22 +64,21 @@ $sql="INSERT INTO users (full_name, email, user_name, password)
 
 $stmt = $db->prepare($sql);
 
-$full_name = 'Juan Perez';
-$email = 'juan.perez@segic.cl';
-$user_name = 'juan.perez';
-$password = 'juan123';
+foreach($users as $user){
+   $full_name = $user['name'];
+   $email = $user['email'];
+   $user_name = $user['user_name'];
+   $password = password_hash($user['password'], PASSWORD_DEFAULT);
+   
 
-$stmt->bindParam(':full_name',$full_name);
-$stmt->bindParam(':email',$email);
-$stmt->bindParam(':user_name',$user_name);
-$stmt->bindParam(':password',$password);
+   $stmt->bindParam(':full_name',$full_name);
+   $stmt->bindParam(':email',$email);
+   $stmt->bindParam(':user_name',$user_name);
+   $stmt->bindParam(':password',$password);
 
-$stmt -> execute();
+   $stmt -> execute();
+}
 
-//delete
 
-/*$id = 3;
 
-$stmt = $db->prepare("DELETE FROM users WHERE id = $id");
-$stmt->bindparam(':id',$id);
-$stmt->execute();*/
+
