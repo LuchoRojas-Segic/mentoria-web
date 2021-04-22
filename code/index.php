1bd84fdab0991116535c505ddc6ac54c5e5b806a
Luis Rojas <luis.rojas@segic.usach.cl>
2021-04-22 16:52:02 -0400

    Pruebas de php y html

--- code/index.php
+++ code/index.php
@@ -54,12 +54,7 @@
 <!--===============================================================================================-->	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" t<style>
-</style>
-	.msg-form{
-		margin:1em;
-		color:#999999;
-	}ype="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
