3af8815d663a514f43914808a3acc3a248e77685
Luis Rojas <luis.rojas@segic.usach.cl>
2021-05-06 17:55:44 -0400

    Trabajo de Excel

--- code/excel.php
+++ code/excel.php
@@ -17,19 +17,31 @@ $users = $stmt ->fetchAll(PDO::FETCH_ASSOC);
 
 $spreadsheet = new Spreadsheet();
 $sheet = $spreadsheet->getActiveSheet();
-$sheet->setCellValue('A1', '#');
+
+/*$sheet->setCellValue('A1', '#');
 $sheet->setCellValue('B1', 'Id');
 $sheet->setCellValue('C1', 'Nombre');
 $sheet->setCellValue('D1', 'Nombre Usuario');
-$sheet->setCellValue('E1', 'Correo');
+$sheet->setCellValue('E1', 'Correo');*/
+
+$sheet->setCellValueByColumnAndRow(1, 1, '#');
+$sheet->setCellValueByColumnAndRow(2, 1, 'Id');
+$sheet->setCellValueByColumnAndRow(3, 1, 'Nombres');
+$sheet->setCellValueByColumnAndRow(4, 1, 'Usuario');
+$sheet->setCellValueByColumnAndRow(5, 1, 'Correo Usuario');
 
 foreach ($users as $key => $user){
     $fil=$key + 2;
-    $sheet->setCellValue('A'.$fil,$key + 1);
-    $sheet->setCellValue('B'.$fil,$user['id']);
+    $sheet->setCellValue(1, $fil,$key + 1);
+    /*$sheet->setCellValue('B'.$fil,$user['id']);
     $sheet->setCellValue('C'.$fil,$user['full_name']);
     $sheet->setCellValue('D'.$fil,$user['user_name']);
-    $sheet->setCellValue('E'.$fil,$user['email']);    
+    $sheet->setCellValue('E'.$fil,$user['email']); */  
+
+    $sheet->setCellValueByColumnAndRow(2, $fil,$user['id']);
+    $sheet->setCellValueByColumnAndRow(3, $fil,$user['full_name']);
+    $sheet->setCellValueByColumnAndRow(4, $fil,$user['user_name']);
+    $sheet->setCellValueByColumnAndRow(5, $fil,$user['email']);   
 }
 $writer = new Xlsx($spreadsheet);
 header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
