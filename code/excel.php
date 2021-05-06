20557e4f1b21bf265e7b7f24384ba20ffbc37653
Luis Rojas <luis.rojas@segic.usach.cl>
2021-05-06 13:31:57 -0400

    Pruebas de carga de archivo excel

---
+++ code/excel.php
@@ -0,0 +1,37 @@
+<?php
+
+require "util/db.php";
+require "../vendor/autoload.php";
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$db = connectDB();
+
+$sql = "SELECT * FROM users";
+//statement
+
+$stmt = $db->prepare($sql);
+$stmt -> execute();
+$users = $stmt ->fetchAll(PDO::FETCH_ASSOC);
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', '#');
+$sheet->setCellValue('B1', 'Id');
+$sheet->setCellValue('C1', 'Nombre');
+$sheet->setCellValue('D1', 'Nombre Usuario');
+$sheet->setCellValue('E1', 'Correo');
+
+foreach ($users as $key->$user){
+    $fil=$key + 2;
+    $sheet->setCellValue('A'.$fil,$key + 1);
+    $sheet->setCellValue('B'.$fil,$user['id']);
+    $sheet->setCellValue('C'.$fil,$user['full_name']);
+    $sheet->setCellValue('D'.$fil,$user['user_name']);
+    $sheet->setCellValue('E'.$fil,$user['e_mail']);    
+}
+$writer = new Xlsx($spreadsheet);
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+header('Content-Disposition: attachment; filename="usuarios.xlsx"');
+$writer->save('php://output');
