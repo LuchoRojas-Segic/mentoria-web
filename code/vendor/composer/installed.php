4acee3dc03a861ebed730c0e852a3a9285ad7e2c
Luis Rojas <luis.rojas@segic.usach.cl>
2021-05-07 10:06:34 -0400

    Primer Gitignore

---
+++ code/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '684dd3178556d270e1ae01e4100e714fc0163c30',
+    'name' => 'luchazo/phpmvc-framework',
+  ),
+  'versions' => 
+  array (
+    'luchazo/phpmvc-framework' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '684dd3178556d270e1ae01e4100e714fc0163c30',
+    ),
+  ),
+);
