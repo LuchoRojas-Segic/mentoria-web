418025e548900159de3c3b22602461fa3a5334d6
Luis Rojas <luis.rojas@segic.usach.cl>
2021-05-11 14:41:57 -0400

    Router

---
+++ code/core/Request.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\core;
+
+class Request
+{
+    public function getPath()
+    {
+        $path = $_SERVER['REQUEST_URI'] ?? '/';        
+        $position = strpos($path, '?');        
+
+        //path user?id=3&otravariable=6
+
+        if ($position === false){
+            return $path;
+        }
+        return substr($path,0,$position);
+    }
+
+    public function getMethod()
+    {
+        return strlower($_SERVER["REQUEST_METHOD"]);
+    }
+}
