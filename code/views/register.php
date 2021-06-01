cd62909128593ebea381fcee2d7f01b798d39817
Luis Rojas <luis.rojas@segic.usach.cl>
2021-06-01 10:22:17 -0400

    Se commienza a implementar controlador auth

--- code/views/register.php
+++ code/views/register.php
@@ -13,11 +13,11 @@
     <input type="email" name = "email" class="form-control">
   </div>  
   <div class="mb-3">
-    <label class="form-label">Pasword</label>
+    <label class="form-label">Password</label>
     <textarea name="password" name = "password" class="form-control" id="" cols="30" rows="10"></textarea>     
   </div>
   <div class="mb-3">
-    <label class="form-label">Confirm Pasword</label>
+    <label class="form-label">Confirm Password</label>
     <textarea name="password" name = "confirmPassword" class="form-control" id="" cols="30" rows="10"></textarea>     
   </div>  
 
