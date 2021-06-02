1578943ef43c47046846fd095de6552b376759f0
Luis Rojas <luis.rojas@segic.usach.cl>
2021-06-02 16:21:48 -0400

    Se implementa layout dinamico

--- code/models/RegisterModel.php
+++ code/models/RegisterModel.php
@@ -1,3 +1,12 @@
 <?php
 
-namespace 
+namespace app\models;
+
+class RegisterModel
+{
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $password;
+    public string $confirmPassword;
+}
