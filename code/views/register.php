0122ab5c30947adab2e26d474337d106f65bee4c
Luis Rojas <luis.rojas@segic.usach.cl>
2021-06-02 16:47:12 -0400

    Se implementa layout dinamico

--- code/views/register.php
+++ code/views/register.php
@@ -2,15 +2,15 @@
 <form method="POST">
   <div class="mb-3">
     <label class="form-label">Firstname</label>
-    <input type="text" name = "firstname" class="form-control">
+    <input type="text" name="firstname" class="form-control">
   </div>
   <div class="mb-3">
     <label class="form-label">Lastname</label>
-    <input type="text" name = "lastname" class="form-control">
+    <input type="text" name="lastname" class="form-control">
   </div> 
   <div class="mb-3">
     <label class="form-label">Email</label>
-    <input type="email" name = "email" class="form-control">
+    <input type="email" name="email" class="form-control">
   </div>  
   <div class="mb-3">
     <label class="form-label">Password</label>
@@ -18,7 +18,7 @@
   </div>
   <div class="mb-3">
     <label class="form-label">Confirm Password</label>
-    <input type="password" name = "confirmPassword" class="form-control">     
+    <input type="password" name="confirmPassword" class="form-control">     
   </div>  
 
   <button type="submit" class="btn btn-primary">Save</button>
