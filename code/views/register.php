ba1b814c699e09741a55862595c79d70beb20c71
Luis Rojas <luis.rojas@segic.usach.cl>
2021-06-16 18:26:37 -0400

    Test

--- code/views/register.php
+++ code/views/register.php
@@ -1,14 +1,8 @@
 <h1>Register</h1>
 
 <?php $form = \app\core\widgets\Form::begin('','POST') ?>
-  <div class="row">
-    <div class="col">
-      <?= $form->field($model, 'firstname') ?>
-    </div>
-    <div class="col">
-      <?= $form->field($model, 'lastname') ?>
-    </div>
-  </div>  
+  <?= $form->field($model, 'firstname') ?>
+  <?= $form->field($model, 'lastname') ?>
   <?= $form->field($model, 'email')->mailField() ?>
   <?= $form->field($model, 'password')->passwordField() ?>
   <?= $form->field($model, 'confirmPassword')->passwordField() ?>
