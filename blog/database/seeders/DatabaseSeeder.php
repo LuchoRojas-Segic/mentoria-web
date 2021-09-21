8bcc37e86816f4993e2ffb340dfb7f21da799a2b
Luis Rojas <luis.rojas@segic.usach.cl>
2021-09-21 15:42:37 -0300

    Se genera seeder con mas usuarios post

--- blog/database/seeders/DatabaseSeeder.php
+++ blog/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,15 @@ class DatabaseSeeder extends Seeder
          Category::truncate();
          Post::truncate();
 
-         Post::factory(5)->create();
+         $user = User::factoryt()->create([
+             'name' => 'Juan Perez',
+         ]);
+
+         Post::factory(3)->create([
+             'user_id' => $user->id,
+         ]);
+
+         Post::factory(10)->create();
 
          /*$user = User::factory()->create();
 
