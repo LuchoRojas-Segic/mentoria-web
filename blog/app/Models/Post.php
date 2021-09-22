751399ff5bcaa4150514893255d5337cc7be9705
Luis Rojas <luis.rojas@segic.usach.cl>
2021-09-22 16:33:09 -0300

    Cambiamos relation de user a author

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -28,8 +28,7 @@ class Post extends Model
                              //(modelo que va a devolver esa relations)
     }
 
-    //public function user()
-    public function author()
+    public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
