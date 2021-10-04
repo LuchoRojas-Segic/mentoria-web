a9b1fa5043f1ef36b993ac7ae553a1a1f8114bba
Luis Rojas <luis.rojas@segic.usach.cl>
2021-10-04 19:05:18 -0300

    Ejemplo uso de slot

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -5,8 +5,13 @@
 </x-layout> v-->
 
 <!--Otra forma de realizar un componente -->
-<x-layout>
+<!--<x-layout>
     <x-slot name="content">
         Hola SEGIC!
     </x-slot>
+</x-layout> -->
+
+<!--La forma más comnún de utilizarlo-->
+<x-layout>
+        Hola SEGIC! estoy utilizando la variable slot
 </x-layout>
