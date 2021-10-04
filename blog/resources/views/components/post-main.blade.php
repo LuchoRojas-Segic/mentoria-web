f539426eb4289bbd89416b95456515f8b4c3fd3e
Luis Rojas <luis.rojas@segic.usach.cl>
2021-10-04 20:32:41 -0300

    Se trabaja en el componente post blade

--- blog/resources/views/components/post-main.blade.php
+++ blog/resources/views/components/post-main.blade.php
@@ -23,7 +23,7 @@
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                            Published <time>{{ $post->created_at }}</time>
+                            Published <time>{{ $post->created_at->diffForHummans() }}</time>
                         </span>
                 </div>
             </header>
