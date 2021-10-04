5cfc09507b95d658c3d3fcddb28260eb8cd5aacf
Luis Rojas <luis.rojas@segic.usach.cl>
2021-10-04 19:00:30 -0300

    Uso de x-slot

--- blog/resources/views/posts.blade.php
+++ blog/resources/views/posts.blade.php
@@ -1,3 +1,12 @@
-<x-layout content="Hola SEGIC!">
+<!-- Primera forma de utilizar un componente
+    <x-layout content="Hola SEGIC!">
 
+    </x-slot>
+</x-layout> v-->
+
+<!--Otra forma de realizar un componente -->
+<x-layout>
+    <x-slot name="content">
+        Hola SEGIC!
+    </x-slot>
 </x-layout>
