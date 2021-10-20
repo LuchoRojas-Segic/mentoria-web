f08f058f3c7128507bea34cd71510d708c41ec71
Luis Rojas <luis.rojas@segic.usach.cl>
2021-10-20 18:50:50 -0300

    Aplicamos clases css al botón

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -31,7 +31,7 @@
                     @foreach ($categories as $category)
                     <a href="/category/{{ $category->slug}}" 
                         class ="block text-left px-3 text-sm leading-6 hover:bg-blue-500 hover:text-white
-                               {{ iiset($currentCategory) && $currentCategory->id === $category->id ? 'bg-blue-500 text-white' : '' }}">
+                               {{ iiset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}">
                         {{ ucwords($category->name) }}
                     </a>
                     @endforeach
