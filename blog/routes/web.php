2bbef4e2c311ca54800717c4e36fe87838324dc1
Luis Rojas <luis.rojas@segic.usach.cl>
2021-10-20 18:41:05 -0300

    Aplicamos clases css al botón

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -119,6 +119,7 @@ Route::get('/category/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts->load(['category', 'author']),
         'categories' => Category::all(),
+        'currentCategory' => $category,
     ]);
 });
 
