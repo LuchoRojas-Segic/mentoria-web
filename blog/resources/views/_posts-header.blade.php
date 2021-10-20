4044eec56835e4276cb2380f7f1fd7f2ec1a060f
Luis Rojas <luis.rojas@segic.usach.cl>
2021-10-20 17:56:15 -0300

    Aplicamos clases css al botón

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -18,10 +18,10 @@
                 <button @click="show = !show" class="py-2 pl-3 pr-9 text-sm font-semibold inline-flex">
                     Category
                 </button>
-                <div x-show="show" class = "py-2 absolute bg-gray-100 w-full mt-2 rounded-xl">
-                    <a href="#" class ="block">One</a>
-                    <a href="#" class ="block">Two</a>
-                    <a href="#" class ="block">Three</a>
+                <div x-show="show" class = "py-2 absolute bg-gray-100 w-full mt-2 rounded-xl z-50">
+                    <a href="#" class ="block text-left px-3 text-sm leading-6">One</a>
+                    <a href="#" class ="block text-left px-3 text-sm leading-6">Two</a>
+                    <a href="#" class ="block text-left px-3 text-sm leading-6">Three</a>
                 </div>
 
             </div>
