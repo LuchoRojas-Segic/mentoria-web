d673f0ca486c36d268b3c4c2673848c6dc6a686f
Luis Rojas <luis.rojas@segic.usach.cl>
2021-11-03 17:04:07 -0300

    Se reemplaza código _post-header por error no encontrado

--- blog/resources/views/_posts-header.blade.php
+++ blog/resources/views/_posts-header.blade.php
@@ -3,39 +3,45 @@
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
 
-    <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
+     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <x-dropdown>
-                <x-slot name = "trigger">
-                <button class="py-2 pl-3 pr-9 text-sm font-semibold  lg:w-32 w-full flex lg:inline-flex">                    
-
-                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : "Categories" }}
-
-                        <x-icon name="down-arrow" class = "absolute pointer-events-none" style="right: 12px"/>
-                 
+        <x-dropdown>
+                <x-slot name="trigger">
+                    <button class="py-2 pl-3 pr-9 text-sm font-semibold  lg:w-32 w-full flex lg:inline-flex">
+                        {{ isset( $currentCategory ) ? ucwords($currentCategory->name) : "Categories"}}
+                        
+                        <x-icon name="down-arrow" class="absolute pointer-events-none" style="right:12px " />
+                        
                     </button>
+
                 </x-slot>
 
-                <x-dropdown-item href="/" :actived="request()->routeIs('home')"> ALL</x-dropdown-item>
                 
-                @foreach ($categories as $category)                         
+
+                <x-dropdown-item href="/" :actived="request()->routeIs('home')">
+                     All
+                </x-dropdown-item>
+
+                @foreach ($categories as $category)
                     <x-dropdown-item 
                         href="/category/{{ $category->slug }}" 
-                        :actived= "isset($currentCategory) && $currentCategory->is($category)" 
+                        :actived="isset($currentCategory) && $currentCategory->is($category)"
                         > {{ ucwords($category->name) }}
                     </x-dropdown-item>
-                @endforeach                                
+                @endforeach
 
-            </x-dropdown>            
-
+            </x-dropdown>
         </div>
 
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="#">
-                <input type="text" name="search" placeholder="Find something"
-                        class="bg-transparent placeholder-black font-semibold text-sm">
+                <input type="text" 
+                        name="search" 
+                        placeholder="Find something"
+                        class="bg-transparent placeholder-black font-semibold text-sm"
+                        value ="{{ request('search') }}">
             </form>
         </div>
     </div>
